5b9e795d5c08d49bb0ef83f61d648f7d2a5a8ef4
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-09-19 19:41:22 -0400

    id added

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,16 +1,16 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Manas Aggrawal Section 3</h1>
+      <h1 id='wd-name'>Manas Aggrawal Section 3</h1>
       <nav>
         <ul>
-          <li>
+          <li id='wd-kanbas'>
             <a href='#/kanbas'>Kanbas</a>
           </li>
-          <li>
+          <li id='wd-labs'>
             <a href='#/labs'>Labs</a>
           </li>
-          <li>
+          <li id='wd-github'>
             <a href='https://github.com/manas-aggrawal/kanbas-react-web-app'>
               Github Kanbas repo
             </a>
