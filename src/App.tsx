4802a928f2f73d2be4a7319fd79d148335cf8d43
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-10-31 18:31:21 -0400

    a4 homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import store from "./Kanbas/store";
 import { Provider } from "react-redux";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Homepage";
 export default function App() {
   return (
     <HashRouter>
       <Provider store={store}>
         <div>
           <Routes>
-            <Route path='/' element={<Navigate to='Labs/Lab4' />} />
+            <Route path='/' element={<HomePage />} />
             <Route path='/Labs/*' element={<Labs />} />
             <Route path='/Kanbas/*' element={<Kanbas />} />
           </Routes>
