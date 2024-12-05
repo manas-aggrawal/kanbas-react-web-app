d3ad77562cbd5de825f09105487752a16b0eb565
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-12-05 16:28:18 -0500

    a6

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -19,6 +19,9 @@ export default function Dashboard({
   addNewCourse,
   deleteCourse,
   updateCourse,
+  enrolling,
+  setEnrolling,
+  updateEnrollment,
 }: {
   courses: any[];
   setCurrCourses: (courses: []) => void;
@@ -27,6 +30,9 @@ export default function Dashboard({
   addNewCourse: (course: any) => void;
   deleteCourse: (course: any) => void;
   updateCourse: (course: any) => void;
+  enrolling: boolean;
+  setEnrolling: (enrolling: boolean) => void;
+  updateEnrollment: (courseId: string, enrolled: boolean) => void;
 }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const navigate = useNavigate();
@@ -47,7 +53,6 @@ export default function Dashboard({
   const isEnrolling = isEnrollingFromStore;
 
   const handleEnroll = async (courseId: string) => {
-    console.log("handle enroll: ", currentUser);
     dispatch(enrollCourse({ userId: currentUser._id, courseId }));
     await enrollmentsClient.enrollUser({
       user: currentUser._id,
@@ -75,6 +80,7 @@ export default function Dashboard({
     const fetchedEnrollments = await enrollmentsClient.fetchEnrollments(
       currentUser._id
     );
+
     dispatch(setEnrollments(fetchedEnrollments));
     setCurrEnrollments(fetchedEnrollments);
     return fetchedEnrollments;
@@ -93,7 +99,15 @@ export default function Dashboard({
 
   return (
     <div id='wd-dashboard'>
-      <h1 id='wd-dashboard-title'>Dashboard</h1>
+      <h1 id='wd-dashboard-title'>
+        Dashboard
+        <button
+          onClick={() => setEnrolling(!enrolling)}
+          className='float-end btn btn-primary'
+        >
+          {enrolling ? "My Courses" : "All Courses"}
+        </button>
+      </h1>
       {userRole === "FACULTY" && (
         <div className='card p-3 mb-4'>
           <h5 className='mb-3'>
@@ -178,6 +192,15 @@ export default function Dashboard({
                     />
                     <div className='card-body'>
                       <h5 className='wd-dashboard-course-title card-title'>
+                        {enrolling && (
+                          <button
+                            className={`btn ${
+                              course.enrolled ? "btn-danger" : "btn-success"
+                            } float-end`}
+                          >
+                            {course.enrolled ? "Unenroll" : "Enroll"}
+                          </button>
+                        )}
                         {course.name}
                       </h5>
                       <p
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -16,29 +16,70 @@ const Kanbas = () => {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const [courses, setCurrCourses] = useState([] as any[]);
   const [enrollments, setCurrEnrollments] = useState([] as any[]);
-
-  const dispatch = useDispatch();
-
-  const fetchCourses = async () => {
-    let courses = [];
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+  const findCoursesForUser = async () => {
     try {
-      courses = await userClient.findMyCourses();
+      const courses = await userClient.findCoursesForUser(currentUser._id);
+
+      setCourses(courses);
     } catch (error) {
       console.error(error);
     }
-    dispatch(setCourses(courses));
-    setCurrCourses(courses);
   };
 
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    if (enrolled) {
+      await userClient.enrollIntoCourse(currentUser._id, courseId);
+    } else {
+      await userClient.unenrollFromCourse(currentUser._id, courseId);
+    }
+    setCourses(
+      courses.map((course) => {
+        if (course._id === courseId) {
+          return { ...course, enrolled: enrolled };
+        } else {
+          return course;
+        }
+      })
+    );
+  };
+
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      console.log("yeh hai enrolled Courses: ", enrolledCourses);
+      const courses = allCourses.map((course: any) => {
+        if (enrolledCourses.find((c: any) => c._id === course._id)) {
+          return { ...course, enrolled: true };
+        } else {
+          return course;
+        }
+      });
+      dispatch(setCourses(courses));
+      setCurrCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
 
   const addNewCourse = async (course: {
     name: string;
     description: string;
   }) => {
-    const newCourse = await userClient.createCourse(course);
+    const newCourse = await courseClient.createCourse(course);
 
     const updatedCourses = [...courses, newCourse];
     dispatch(setCourses(updatedCourses));
@@ -100,6 +141,9 @@ const Kanbas = () => {
                           addNewCourse={addNewCourse}
                           deleteCourse={deleteCourse}
                           updateCourse={updateCourse}
+                          enrolling={enrolling}
+                          setEnrolling={setEnrolling}
+                          updateEnrollment={updateEnrollment}
                         />
                       </ProtectedRoute>
                     }
